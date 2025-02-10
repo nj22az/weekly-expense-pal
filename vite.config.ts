aebcb38a6ebbb9cd29d48fdcc5e8c6798670a2bc
nj22az <nils.johansson@me.com>
2025-02-10 10:08:07 +0100

    Update configuration files

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,11 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
   },
   plugins: [react()],
   resolve: {
